9a90f8f264868122e2f607ce7e2110c3b38d0394
amitmistry24 <50727986+amitmistry24@users.noreply.github.com>
2019-05-17 14:22:15 -0400

    Add files via upload

---
+++ docs/best_buy-ecommerce_app/src/app.js
@@ -0,0 +1,44 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch(
+  'IVNRBZTFI1',
+  '39b5cee90079e0d74a2dfedeeafce59d'
+);
+
+const search = instantsearch({
+  indexName: 'best_buy',
+  searchClient,
+});
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+        <article>
+          <h1>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h1>
+          <p>{{#helpers.highlight}}{ "attribute": "shortDescription" }{{/helpers.highlight}}</p>
+          <p>{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</p>
+          <p>{{#helpers.highlight}}{ "attribute": "salePrice" }{{/helpers.highlight}}</p>
+          <p>{{#helpers.highlight}}{ "attribute": "price" }{{/helpers.highlight}}</p>
+        </article>
+      `,
+    },
+  })
+);
+
+
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  })
+);
+
+search.start();
